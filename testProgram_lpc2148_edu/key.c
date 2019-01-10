053e3da546c88ec657174926f0e5bf58be48ec85
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2019-01-10 10:31:38 +0100

    New joystick

--- testProgram_lpc2148_edu/key.c
+++ testProgram_lpc2148_edu/key.c
@@ -79,8 +79,8 @@ getKeys(void)
 tU8
 checkKeyUpDown(void)
 {
-  if ((IOPIN & KEYPIN_UP) == 0)     return 1;
-  if ((IOPIN & KEYPIN_DOWN) == 0)   return -1;
+  if ((IOPIN & KEYPIN_RIGHT) == 0)     return 1;
+  if ((IOPIN & KEYPIN_LEFT) == 0)   return -1;
   return 0;
 }
 
